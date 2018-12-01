06abfbd1f0161db1e20575d81a40354f23c47ade
Wojciech Kicior <Wojciech.Kicior@gmail.com>
2018-12-01 22:22:57 +0100

    initialize empty project

---
+++ src/main/java/com/gearfound/gearfoundauthorizationservice/GearfoundAuthorizationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.gearfound.gearfoundauthorizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GearfoundAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GearfoundAuthorizationServiceApplication.class, args);
+	}
+}
